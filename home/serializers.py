4bbd619e62a46ffa7e982dc154e4c141bd7a3ac3
Gowtham Pollam <gowtham.pollam2018@vitstudent.ac.in>
2020-05-04 21:12:23 +0530

    Update serializers.py

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import world
+from .models import world,countries,states
 
 class WorldSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model= world
-        fields=('country','total_cases','total_deaths','total_recovered','new_cases','new_deaths')
+        fields=('total_cases','deaths','recovered','active')
+
+class CountriesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=countries
+        fields=('country','total_cases','deaths','recovered','active','new_cases','new_deaths')
+
+class StatesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        moddel=states
+        fields=('state','total_cases','deaths','recovered')
