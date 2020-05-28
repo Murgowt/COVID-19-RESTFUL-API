c167a00bb3b53ff000307883f2319cfd9b2dd130
Gowtham Pollam <gowtham.pollam2018@vitstudent.ac.in>
2020-05-28 16:05:39 +0530

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import world
+from .models import world,countries,states
 # Register your models here.
 
 admin.site.register(world)
-
+admin.site.register(countries)
+admin.site.register(states)
