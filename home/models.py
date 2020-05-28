ccc6948b5983b6df44e202d065036116dee3d3d0
Gowtham Pollam <gowtham.pollam2018@vitstudent.ac.in>
2020-05-28 16:04:45 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -3,9 +3,24 @@ from django.db import models
 # Create your models here.
 
 class world(models.Model):
-    total_cases=models.IntegerField()
-    deaths=models.IntegerField()
-    recovered=models.IntegerField()
-    active=models.IntegerField()
+    total_cases = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
+    active = models.IntegerField()
+
+class countries(models.Model):
+    country=models.CharField(max_length=20)
+    total_cases = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
+    active = models.IntegerField()
+    new_cases=models.IntegerField()
+    new_deaths=models.IntegerField()
+
+class states(models.Model):
+    state = models.CharField(max_length=30)
+    total_cases = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
 
     
