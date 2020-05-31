1ef476fb31a652ee2108e7ca77c4afb18c1a736e
Gowtham Pollam <gowtham.pollam2018@vitstudent.ac.in>
2020-05-31 18:25:49 +0530

    Update urls.py

--- COVID/urls.py
+++ COVID/urls.py
@@ -20,8 +20,9 @@ from rest_framework import routers
 
 router=routers.DefaultRouter()
 router.register(r'world',HV.WorldViewSet)
+router.register(r'countries',HV.CountriesViewSet)
+router.register(r'states',HV.StatesViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
-    path('api-auth/',include('rest_framework.urls',namespace='rest_framework'))
 ]
